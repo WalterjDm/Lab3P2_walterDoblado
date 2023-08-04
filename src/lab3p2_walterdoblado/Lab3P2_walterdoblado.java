0073159d08cacda792073ee7ae1a0995714c43ee
WalterjDm <walterjdoblado569@unitec.edu>
2023-08-04 13:52:16 -0600

    menu avance

--- src/lab3p2_walterdoblado/Lab3P2_walterdoblado.java
+++ src/lab3p2_walterdoblado/Lab3P2_walterdoblado.java
@@ -1,14 +1,30 @@
 
 package lab3p2_walterdoblado;
 
+import java.util.Scanner;
+
 
 public class Lab3P2_walterdoblado {
 
   
     public static void main(String[] args) {
-   
+   Scanner lea = new Scanner(System.in);
+        int opc = 0;
+        String opc1;
         
-        
+        while (opc != 8) {
+             System.out.print("seleccionar: ");
+            opc1 = lea.next();
+            while (!(opc1.matches("\\d{1,1}"))) {
+                System.out.println("no ingreso un numero o el numero esta fuera del rango");
+                System.out.print("seleccionar: ");
+                opc1 = lea.next();
+            }
+            opc = Integer.parseInt(opc1);
+            
+            System.out.println(opc);
+            
+        }
         
         
         
